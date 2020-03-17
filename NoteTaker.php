85588086fddf3cb97fba8d46686be1895adc2a13
Andrew Martin <andy123@stanford.edu>
2020-03-17 11:36:18 -0700

    setting up config.json

--- NoteTaker.php
+++ NoteTaker.php
@@ -1,15 +1,21 @@
 <?php
+declare(strict_types=1);
 namespace Stanford\NoteTaker;
 
-class NoteTaker extends \ExternalModules\AbstractExternalModule {
-	public function __construct() {
-		parent::__construct();
-		// Other code to run when object is instantiated
-	}
-	
-	public function redcap_save_record( int $project_id, string $record = NULL, string $instrument, int $event_id, int $group_id, string $survey_hash = NULL, int $response_id, int $repeat_instance = 1 ) {
-		
-	}
+require_once("emLoggerTrait.php");
 
-	
+
+class NoteTaker extends \ExternalModules\AbstractExternalModule {
+
+  use emLoggerTrait;
+
+  public function __construct() {
+    parent::__construct();
+    // Other code to run when object is instantiated
+  }
+
+  public function redcap_save_record( int $project_id, string $record, string $instrument, int $event_id, int $group_id, string $survey_hash, int $response_id, int $repeat_instance = 1 ) {
+
+  }
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NoteTaker
 A quick EM that allows comments to be added to a single notes field, formatted with user and timestamp.
+
+
+
