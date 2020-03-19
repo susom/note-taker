94d7118a3906420efe80b57321e187544b7d3349
Jordan Schultz <jmschultz@ucdavis.edu>
2020-03-18 17:02:02 -0700

    initial commit to NoteTaker EM, provides basic functionality and configuration options + readme

--- NoteTaker.php
+++ NoteTaker.php
@@ -15,29 +15,37 @@ class NoteTaker extends \ExternalModules\AbstractExternalModule {
   }
 
 
-  public function redcap_save_record($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash, $response_id, $repeat_instance = 1 )
-  {
-    // Is a config defined for this instrument?
-    // Take the current insturment and get all the fields.  Then check if the 'input field' is present in the instrument fields and is not empty.  If so, then add a log entry...
+  /** Prepends an entry into notebox specified by user upon record save
+   * @param $project_id
+   * @param null $record
+   * @param $instrument
+   * @param $event_id
+   * @param null $group_id
+   * @param $survey_hash
+   * @param $response_id
+   * @param int $repeat_instance
+   * @return bool
+   */
+  public function redcap_save_record($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash, $response_id, $repeat_instance = 1 ){
+    $instances = $this->getSubSettings('instance'); // Take the current insturment and get all the fields.  Then check if the 'input field' is present in the instrument fields and is not empty.  If so, then add a log entry...
 
-    $instances = $this->getSubSettings('instance');
-
-    // Loop over all of them
+    // Loop over all instances
     foreach ($instances as $i => $instance) {
-
+      //grab all names of instances within the instrument
       $i_event_id   = $instance['event-id'];
       $i_date_field = $instance['date-field'];
       $i_note_field = $instance['note-field'];
       $i_input_field = $instance['input-field'];
+      $i_include_delimiter = $instance['include-delimiter'];
 
       $instrument_fields = "";
 
       // Only process this config if the form is in the same event id
-      if ($instance["event-id"] == $event_id) {
+      if ($instance["event-id"] == $i_event_id) {
 
-        // Check if input_field is in $instrument
         if (empty($instrument_fields)) $instrument_fields = REDCap::getFieldNames($instrument);
 
+        // Check if input_field is in $instrument
         if (in_array($instance["input-field"], $instrument_fields)) {
           $this->emDebug($i_input_field . " is on this form! ");
 
@@ -55,71 +63,77 @@ class NoteTaker extends \ExternalModules\AbstractExternalModule {
 
           $this->emDebug($data, "Record $record Data");
 
+          //Check if there is any input to be prepended to notes
           if (!empty($data[$i_input_field])) {
-
-            $this->emDebug("Data before update", $data);
-
-
             // Update
             $this->emDebug("Update Note");
 
-
-            // Prepend input to note
-            // TODO: Only apply delimiter if existing note exists
-            $data[$i_note_field] = "[formatting]" . $data[$i_input_field] . "\n---\n" . $data[$i_note_field];
-
-            // Erase input field
-            $data[$i_input_field] = "";
-
-            // Get the format of the date_field
-            global $Proj;
-            $date_format = $Proj->metadata[$i_date_field]['element_validation_type'];
-            $this->emDebug($i_date_field . " is " . $date_format);
-            switch ($date_format) {
-              case "datetime_ymd":
-                $new_date_format = "Y-m-d H:i:s";
-                break;
-              case "date_ymd":
-                $new_date_format = "Y-m-d";
-                break;
-              default:
-                $this->emDebug($date_format . " is not supported!");
-                return false;
-            }
-
+            //set date field to current time
+            $new_date_format = $this->getNewDateFormat($data,$i_date_field);
             $data[$i_date_field] = Date($new_date_format);
+
+            //Update note box
+            $data = $this->prependInput($data, $i_note_field, $i_input_field, $i_date_field, $i_include_delimiter);
             $this->emDebug("Data after update", $data);
 
             // Save
             $data2_json = json_encode(array($data));
-
             $result = REDCap::saveData('json', $data2_json, 'overwrite');
             $this->emDebug($data_json, $data2_json, $result, "Save Result");
-
           }
         }
       }
     }
   }
 
+  /** Returns the new date format based on value
+   * @param $data
+   * @param $i_date_field
+   * @return String : representation of new date format
+   */
+  private function getNewDateFormat($data, $i_date_field){
+    global $Proj;
+    $date_format = $Proj->metadata[$i_date_field]['element_validation_type'];
+    $this->emDebug($i_date_field . " is " . $date_format);
 
-        // See if this config is valid
-        // $sc = new SummarizeInstance($this, $instance);
-        // $valid = $sc->validateConfig();
+    $tokenized = explode("_", $date_format);
 
-        // // If valid put together the summarize block and save it for this record
-        // $config_num = $i++;
-        // if ($valid) {
-        //   $saved = $sc->saveSummarizeBlock($record, $instrument, $repeat_instance, $this->deleteAction);
-        //   if ($saved) {
-        //     $this->emDebug("Saved summarize block $config_num for record $record and instance $repeat_instance");
-        //   } else {
-        //                 $this->emLog($sc->getErrors());
-        //             }
-        //         } else {
-        //             $this->emError("Skipping Summarize config $config_num for record $record because config is invalid" . json_encode($instance));
-        //         }
+    if(in_array("datetime", $tokenized) && in_array("seconds", $tokenized)) { //dateTime seconds
+      $new_date_format = "Y-m-d H:i:s";
+    } else if(in_array("datetime", $tokenized)){ //dateTime without seconds
+      $new_date_format = "Y-m-d H:i";
+    } else if(in_array("date", $tokenized)){ //Regular date
+      $new_date_format = "Y-m-d";
+    } else {
+      return '';
+    }
+
+    return $new_date_format;
+  }
 
 
+  /** Prepends String based on data object to the note field
+   * @param $data : data object containing the values of instrument instances
+   * @param $i_note_field : note field
+   * @param $i_input_field : input field
+   * @param $i_date_field : date field
+   * @param $i_include_delimiter : T/F, whether to delimit note by a line
+   * @return $data : updated data object
+   **/
+  private function prependInput($data, $i_note_field, $i_input_field, $i_date_field, $i_include_delimiter){
+    if($data){
+      $user = USERID;
+      if(!empty($data[$i_note_field]) && $i_include_delimiter){ //If notes field already has been prepended to
+        $data[$i_note_field] = $data[$i_note_field] . "\n------------------------------------------------------------\n" . "[{$user} @ {$data[$i_date_field]}]\n" . $data[$i_input_field];
+      } else { //New note, no need for delimiter
+        $data[$i_note_field] = $data[$i_note_field] . "\n[{$user} @ {$data[$i_date_field]}]\n" . $data[$i_input_field];
+      }
+
+      // Erase input field
+      $data[$i_input_field] = "";
+      return $data;
+    }
+    $this->emError('Data object supplied is empty');
+  }
 
 }
--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # NoteTaker
-A quick EM that allows comments to be added to a single notes field, formatted with user and timestamp.
+An EM that allows comments to be added to a single notes field, formatted with user and timestamp.
+<br>
+Notes will be arranged newest to oldest and prepended with a header with the following format : [username @ date]
+<br>
+##Configuration options
+The following instrument fields can be specified:
+1. [Arm & Event ID] : The event where the records intended to be affected by this EM on are located
+2. [Input Field] : The input instance you would like to pull text from to be added to the notes field
+3. [Date Field] : The date field that will be populated anytime a user edits the input field. Formats include date, datetime and datetime_seconds
+4. [Notes Field] : The field that contains all input field text separated by a header and optional delimiter
+5. [Include a delimiter] : If checked, will separate each valid record update with a string "______________"
 
+
+##Notes:
+When choosing between validation formats for date in the designer, feel free to choose between the following options<br>
+1. Date
+2. Datetime
+3. Datetime w/seconds
+
+Note however that the header will be formatted in (Y-M-D) regardless of your choice. The only difference being the specificity rather than the order.
+If specifying an altering order, the data will save in (Y-M-D) format, and you might receive an alert message upon record save.
 
 
